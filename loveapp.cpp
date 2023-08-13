791bdf00485551b4852385b6d71ef124f3550814
Coding-Ocean <eijo524@gmail.com>
2023-08-13 12:44:36 +0900

    vertex

--- loveapp.cpp
+++ loveapp.cpp
@@ -1,14 +1,48 @@
-#include<windows.h>
+#include<vector>
 #include"../lovelib/lovelib.h"
+
 void gmain()
 {
-	window("Coding Ocean", 960, 540);
+	int width = 1920, height = 1080;
+	window("Love", width, height);
+	wireframe();
+
+	int vtxShape[] = {
+		createShape(4, 0.3f),
+		createShape(6, 0.3f),
+		createShape(8, 0.3f),
+		createShape(10, 0.5f),
+		createShape(20, 0.8f),
+	};
+
+	float len = 250;
+	float ofstx = len;
+	float px = ofstx;
+	float py = height / 2.0f;
+	float angle = 0;
+	float alpha = 1;
+	
 	while (!quit()) {
 		if (escKeyPressed()) {
 			closeWindow();
 		}
-		clear(0.3f, 0.6f, 1.0f);
-		
+
+		clear(0.0f, 0.0f, 0.0f);
+
+		angle += 0.01f;
+
+		fill(1, 0.8f, 0, alpha);
+		px = ofstx;
+		rect(px, py, len/1.4f, len/1.4f, angle);
+
+		px += ofstx;
+		circle(px, py, len);
+
+		for (int i = 0; i < 5; i++) {
+			px += ofstx;
+			shape(vtxShape[i], px, py, len, len, angle);
+		}
+
 		present();
 	}
 }

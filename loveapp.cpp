d1dc6ffbc9594d4d2b1a61cd01535f6dfd61cfb1
Coding-Ocean <eijo524@gmail.com>
2023-08-17 17:38:29 +0900

    primitive sphere

--- loveapp.cpp
+++ loveapp.cpp
@@ -1,61 +1,5 @@
 #include"../lovelib/lovelib.h"
-
-int createVtxCube()
-{
-	float l = 0.5f;
-	VERTEX vertices[] = {
-		//ê≥ñ 
-		-l, l,-l, 0,0,-1, 0,0,
-		-l,-l,-l, 0,0,-1, 0,1,
-		 l,-l,-l, 0,0,-1, 1,1,
-		 l, l,-l, 0,0,-1, 1,0,
-		//ó†ñ 
-		 l, l, l, 0,0,1, 0,0,
-		 l,-l, l, 0,0,1, 0,1,
-		-l,-l, l, 0,0,1, 1,1,
-		-l, l, l, 0,0,1, 1,0,
-		//âEñ 
-		 l, l,-l, 1,0,0, 0,0,
-		 l,-l,-l, 1,0,0, 0,1,
-		 l,-l, l, 1,0,0, 1,1,
-		 l, l, l, 1,0,0, 1,0,
-		//ç∂ñ 
-		-l, l, l, -1,0,0, 0,0,
-		-l,-l, l, -1,0,0, 0,1,
-		-l,-l,-l, -1,0,0, 1,1,
-		-l, l,-l, -1,0,0, 1,0,
-		//è„ñ 
-		-l, l, l, 0,1,0, 0,0,
-		-l, l,-l, 0,1,0, 0,1,
-		 l, l,-l, 0,1,0, 1,1,
-		 l, l, l, 0,1,0, 1,0,
-		//â∫ñ 
-		-l,-l,-l, 0,-1,0, 0,0,
-		-l,-l, l, 0,-1,0, 0,1,
-		 l,-l, l, 0,-1,0, 1,1,
-		 l,-l,-l, 0,-1,0, 1,0,
-	};
-	return createVertexBuffer(vertices, _countof(vertices));
-}
-
-int createIdxCube()
-{
-	unsigned short indices[] = {
-		0,1,2,
-		0,2,3,
-		4,5,6,
-		4,6,7,
-		8,9,10,
-		8,10,11,
-		12,13,14,
-		12,14,15,
-		16,17,18,
-		16,18,19,
-		20,21,22,
-		20,22,23,
-	};
-	return createIndexBuffer(indices, _countof(indices));
-}
+#include"../lovelib/primitive.h"
 
 void gmain()
 {
@@ -66,24 +10,37 @@ void gmain()
 	int idxCube = createIdxCube();
 	int texCube = loadImage("tex1.png");
 
+	int vtxSphere = createVtxSphere(30,1);
+	int idxSphere = createIdxSphere(30);
+	int texSphere = loadImage("earth.png");
+
+	MAT world;
 	float angle = 0;
 
-	setLightDirection(0, 0, 1);
+	setLightDirection(-1, -0.5f, 1);
 	setAmbient(0.2f, 0.2f, 0.2f);
-
+	
 	while (!quit()) {
 		if (escKeyPressed()) closeWindow();
 		
 		clear(0.0f, 0.0f, 0.0f);
 		
 		angle += 0.008f;
-		
-		//3D
-		MAT world;
+
+		//Cube
 		world.identity();
 		world.mulRotateX(angle);
+		world.mulRotateY(angle);
 		world.mulRotateZ(angle);
-		model(vtxCube, idxCube, texCube, world);
+		world.mulTranslate(1, 0, 0);
+		//model(vtxCube, idxCube, texCube, world);
+
+		//Sphere
+		world.identity();
+		world.mulRotateY(-angle);
+		world.mulRotateZ(-0.4f);
+		world.mulTranslate(0, 0, 0);
+		model(vtxSphere, idxSphere, texSphere, world);
 
 		present();
 	}

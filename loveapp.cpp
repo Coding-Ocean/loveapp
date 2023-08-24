143f0a72930e698c93eb3cfaa39c444d752e7335
Coding-Ocean <eijo524@gmail.com>
2023-08-24 18:05:51 +0900

    add cross

--- loveapp.cpp
+++ loveapp.cpp
@@ -1,52 +1,4 @@
-#if 1
-#include"../lovelib/lovelib.h"
-#include"../lovelib/primitive.h"
-#include<cmath>
-void gmain()
-{
-	int width = 1920, height = 1080;
-	window("Love", width, height);
-
-	int numCorners = 4;
-	int vtx = createVtxCylinder(0.02f,numCorners);
-	int idx = createIdxCylinder(numCorners);
-	int tex = 0;// loadImage("earth.png");
-
-	int vtxSphere = createVtxSphere(0.02f, numCorners);
-	int idxSphere = createIdxSphere(numCorners);
-
-	MAT world;
-	float angle = 0;
-
-	while (!quit()) {
-		if (escKeyPressed()) closeWindow();
-
-		clear(0.f, 0.f, 0.f);
-
-		angle += 0.01f;
-
-		//ambient(0.4f, 0.4f, 0.4f);
-		
-		VEC p(1, -1, -0.3f);
-		world.identity();
-		fill(1, 1, 0);
-		model(vtxSphere, idxSphere, 0, world);
-		world.mulTranslate(p.x, p.y, p.z);
-		model(vtxSphere, idxSphere, 0, world);
-		
-		world.identity();
-		world.mulScaling(p.mag(), 1, 1);
-		VEC n = normalize(p);
-		world.mulRotateZ(acos(-n.y) - 1.57f);
-		world.mulRotateY(-atan2(p.z, p.x));
-		fill(1, 1, 0);
-		model(vtx, idx, tex, world);
-
-		present();
-	}
-}
-#endif
-
+//éOäpÉ|ÉäÉSÉì
 #if 0
 #include"../lovelib/lovelib.h"
 
@@ -74,19 +26,20 @@ void gmain()
 
 	int vtx = createVtxTriangle(0.5f);
 	int idx = createIdxTriangle();
-	int tex = loadImage("tex2.png");
+	int tex = loadImage("earth.png");
 
 	MAT world;
 	float angle = 0;
-
+	float scale = 1;
+	
 	while (!quit()) {
 		if (escKeyPressed()) closeWindow();
 
-		clear(0.4f, 0.4f, 0.4f);
+		clear(0.6f, 0.2f, 0.2f);
 
 		angle += 0.01f;
-
 		world.identity();
+		world.mulScaling(scale, scale, scale);
 		world.mulRotateY(angle);
 		world.mulTranslate(0, 0, 0);
 		model(vtx, idx, tex, world);
@@ -95,7 +48,7 @@ void gmain()
 	}
 }
 #endif
-
+//ínãÖÇ∆óßï˚ëÃ
 #if 0
 #include"../lovelib/lovelib.h"
 #include"../lovelib/primitive.h"
@@ -105,6 +58,7 @@ void gmain()
 {
 	int width = 1920, height = 1080;
 	window("Love", width, height);
+	hideCursor();
 
 	//Cube
 	int vtxCube = createVtxCube();
@@ -116,8 +70,6 @@ void gmain()
 	int vtxSphere = createVtxSphere(radius, numCorners);
 	int idxSphere = createIdxSphere(numCorners);
 	int texSphere = loadImage("earth.png");
-	//Logo
-	int texLogo = loadImage("logo.png");
 
 	MAT world;
 	float angle = 0;
@@ -129,12 +81,6 @@ void gmain()
 		
 		angle += 0.008f;
 
-		//Sphere
-		world.identity();
-		world.mulRotateY(-angle);
-		world.mulRotateX(-0.5f);
-		model(vtxSphere, idxSphere, texSphere, world);
-		
 		//Cube
 		world.identity();
 		world.mulRotateZ(angle);
@@ -142,22 +88,27 @@ void gmain()
 		world.mulRotateY(angle);
 		model(vtxCube, idxCube, texCube, world);
 
-		//Logo
-		image(texLogo, width/2.0f, height/2.0f);
+		//Sphere
+		world.identity();
+		world.mulRotateY(-angle);
+		world.mulRotateX(-0.5f);
+		model(vtxSphere, idxSphere, texSphere, world);
 
 		present();
 	}
 }
 #endif
-
+//É\Å[ÉâÅ[ÉVÉXÉeÉÄ
 #if 0
 #include"../lovelib/lovelib.h"
 #include"../lovelib/primitive.h"
+#include<cmath>
 
 void gmain()
 {
 	int width = 1920, height = 1080;
 	window("Love", width, height);
+	hideCursor();
 
 	int numCorners = 50;
 	int vtxSphere = createVtxSphere(0.5f,numCorners);
@@ -181,13 +132,14 @@ void gmain()
 		clear(0.0f, 0.0f, 0.0f);
 
 		angle += rotationSpeed;
+
 		//ëæóz-------------------------------------------------------
 		world.identity();
 		world.mulScaling(3.f, 3.f, 3.f);
 		//ëæózÇÃé©ì]
 		world.mulRotateY(-angle*0.3f);
 		//ëæózÇÃï`âÊ
-		ambient(1.f, 1.f, 1.f);
+		ambient(1, 1, 1);
 		model(vtxSphere, idxSphere, texSun, world);
 
 		//ínãÖ-------------------------------------------------------
@@ -199,7 +151,7 @@ void gmain()
 		world.mulRotateY(-angle);
 		//ínãÖÇÃï`âÊ
 		setLightDirection(world._41, world._42, world._43);
-		ambient(0.f, 0.f, 0.f);
+		ambient(0, 0, 0);
 		model(vtxSphere, idxSphere, texEarth, world);
 
 		//åé---------------------------------------------------------
@@ -213,9 +165,83 @@ void gmain()
 		world.mulRotateY(-angle);
 		//åéï`âÊ
 		model(vtxSphere, idxSphere, texMoon, world);
+		
+		present();
+	}
+}
+#endif
+//â~íå
+#if 0
+#include"../lovelib/lovelib.h"
+#include<cmath>
+void gmain()
+{
+	int width = 1920, height = 1080;
+	window("Love", width, height);
+	hideCursor();
+
+	int numCorners = 30;
+	int vtx = createVtxCylinder(0.5f/*radius*/, numCorners, -0.6f/*low*/, 0.6f/*high*/);
+	int idx = createIdxCylinder(numCorners);
+	int tex = loadImage("earth.png");
+
+	MAT world;
+	float angle = 0;
+
+	while (!quit()) {
+		if (escKeyPressed()) closeWindow();
+
+		clear(0.1f, 0.2f, 0.2f);
+
+		angle += -0.01f;
+
+		world.identity();
+		world.mulRotateY(angle);
+		world.mulRotateX(angle);
+		model(vtx, idx, tex, world);
 
 		present();
 	}
 }
 #endif
+//äOêœÉxÉNÉgÉã
+#if 0
+#include"../lovelib/lovelib.h"
+#include<cmath>
+void gmain()
+{
+	int width = 1920, height = 1080;
+	window("Love", width, height);
 
+	createLine3D(0.01f,4);
+
+	MAT world;
+	float angle = 0;
+
+	setView(VEC(3, 1, -3), VEC(0, 0, 0), VEC(0, 1, 0));
+	setLightDirection(-1, -1, 1);
+
+	while (!quit()) {
+		if (escKeyPressed()) closeWindow();
+
+		clear(0.1f, 0.2f, 0.2f);
+
+		angle += -0.01f;
+
+		VEC o{ 0,0,0 };
+		VEC v1{ 1,0,0 };
+		VEC v2{ cos(angle), 0, sin(angle) };
+		VEC v3 = cross(v1, v2);
+
+		ambient(0.4f, 0.4f, 0.4f);
+		fill(1, 0, 0);
+		line3D(o, v1);
+		fill(0, 1, 0);
+		line3D(o, v2);
+		fill(1, 1, 0);
+		line3D(o, v3);
+
+		present();
+	}
+}
+#endif

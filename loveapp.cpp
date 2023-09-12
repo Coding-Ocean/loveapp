d5e45b1bc892f50c8fccae77df8035415156ca31
Coding-Ocean <eijo524@gmail.com>
2023-09-12 16:10:24 +0900

    mod mathFunctions

--- loveapp.cpp
+++ loveapp.cpp
@@ -12,78 +12,79 @@ void mathAxis(float originX, float originY, float scale, float thickness)
 	Oy = originY;
 	Scl = scale;
 
+	//ê¸ÇÃëæÇ≥
+	float sw = Scl * thickness;
+
 	//é≤
 	//x
-	line(0, Oy, width, Oy, Scl * thickness);
+	line(0, Oy, width, Oy, sw);
 	//y
-	line(Ox, 0, Ox, height, Scl * thickness);
+	line(Ox, 0, Ox, height, sw);
 
 	//ñ⁄ê∑ÇË
 	//x
-	float l = Scl * thickness * 2;
-	int num = 0;
-	num = (width-Ox) / Scl;
-	for (int x = 1; x <= num; x++) {
-		line(Ox + Scl * x, Oy - l, Ox + Scl * x, Oy + l, Scl * thickness);
+	float l = Scl * thickness * 2.5f;
+	int num, i;
+	float x, y;
+	num = int((width-Ox) / Scl);
+	for (i = 1; i <= num; i++) {
+		x = Ox + Scl * i;
+		line(x, Oy - l, x, Oy + l, sw);
 	}
-	num = Ox / Scl;
-	for (int x = 1; x <= num; x++) {
-		line(Ox + Scl * -x, Oy - l, Ox + Scl * -x, Oy + l, Scl * thickness);
+	num = int(Ox / Scl);
+	for (i = 1; i <= num; i++) {
+		x = Ox + Scl * -i;
+		line(x, Oy - l, x, Oy + l, sw);
 	}
 	//y
-	num = (height-Oy) / Scl;
-	for (int y = 1; y <= num; y++) {
-		line(Ox - l, Oy + Scl * y, Ox + l, Oy + Scl * y, Scl * thickness);
+	num = int((height-Oy) / Scl);
+	for (i = 1; i <= num; i++) {
+		y = Oy + Scl * i;
+		line(Ox - l, y, Ox + l, y, sw);
 	}
-	num = Oy / Scl;
-	for (int y = 1; y <= num; y++) {
-		line(Ox - l, Oy + Scl * -y, Ox + l, Oy + Scl * -y, Scl * thickness);
+	num = int(Oy / Scl);
+	for (i = 1; i <= num; i++) {
+		y = Oy + Scl * -i;
+		line(Ox - l, y, Ox + l, y, sw);
 	}
 }
 void mathCircle(float x, float y, float diameter)
 {
-	circle(Ox + Scl * x, Oy - Scl * y, Scl * diameter);
+	circle(Ox + Scl * x, Oy - Scl * y, Scl * diameter, 10);
 }
 void mathLine(float sx, float sy, float ex, float ey, float thickness)
 {
-	line(Ox + Scl * sx, Oy - Scl * sy, Ox + Scl * ex, Oy - Scl * ey, Scl * thickness);
+	line(Ox + Scl * sx, Oy - Scl * sy, Ox + Scl * ex, Oy - Scl * ey, Scl * thickness, 10);
 }
 void mathGraph(float (*func)(float), float inc, float thickness, float diameter)
 {
-	float minX = -(Ox / Scl);
-	float maxX = (width - Ox) / Scl;
-	float sx, sy, ex, ey;
 	//ê≥
-	for (sx = 0; sx < maxX; sx += inc) {
-		sy = func(sx);
-		ex = sx + inc;
-		ey = func(ex);
-		mathLine(sx, sy, ex, ey, thickness);
-		mathCircle(sx, sy, diameter);
+	float maxX = (width - Ox) / Scl;
+	for (float x = 0; x < maxX; x += inc) {
+		mathLine(x, func(x), x + inc, func(x + inc), thickness);
+		mathCircle(x, func(x), diameter);
 	}
 	//ïâ
-	for (sx = 0; sx > minX; sx -= inc) {
-		sy = func(sx);
-		ex = sx - inc;
-		ey = func(ex);
-		mathLine(sx, sy, ex, ey, thickness);
-		mathCircle(sx, sy, diameter);
+	float minX = -(Ox / Scl);
+	for (float x = 0; x > minX; x -= inc) {
+		mathLine(x, func(x), x - inc, func(x - inc), thickness);
+		mathCircle(x, func(x), diameter);
 	}
 }
 
-float func0(float x)
-{
-	return x*x;
-}
 float func1(float x)
 {
-	return x * x * x;
+	return x;
 }
 float func2(float x)
 {
-	return sin(x);
+	return x * x;
 }
 float func3(float x)
+{
+	return x * x * x;
+}
+float func4(float x)
 {
 	return cos(x);
 }
@@ -95,7 +96,6 @@ void gmain()
 	float ox = width / 2;
 	float oy = height / 2;
 	float scale = width / 8;
-	float thickness = 0.025f;
 
 	while (!quit()) {
 		getInputState();
@@ -108,44 +108,27 @@ void gmain()
 			ox += mouseVx;
 			oy += -mouseVy;
 		}
-		scale += mouseWheel * 5;
+		scale += mouseWheel * 10;
 		if (scale < 10)scale = 10;
 		fill(0.8f, 0.8f, 0.8f);
-		mathAxis(ox, oy, scale, thickness);
+		mathAxis(ox, oy, scale, 0.01f);
 		
-
-		float increments = 0.2f;
-		float thickness = 0.05f;
-		float plotsize = thickness;
+		//ä÷êîÉOÉâÉt
+		float increments = 0.05f;
+		float thickness = 0.02f;
+		float plotsize = 0.02f;
 
 		fill(0.6f, 0.6f, 0.962f);
-		mathGraph(func0, increments, thickness, plotsize);
+		mathGraph(func1, increments, thickness, plotsize);
 		
-		fill(1, 0.952f, 0.247f);
-		mathGraph(func1, increments*0.5f, thickness, plotsize);
+		fill(1.0f, 0.952f, 0.247f);
+		mathGraph(func2, increments*0.5f, thickness, plotsize);
 
 		fill(0.913f, 0.329f, 0.419f);
-		mathGraph(func2, increments, thickness, plotsize);
-
-		fill(0, 0.678f, 0.662f);
 		mathGraph(func3, increments, thickness, plotsize);
 
-		//â~
-		fill(0.913f, 0.529f, 0.119f);
-		float radius = 1.5f;
-		float sx = radius, sy = 0, ex = 0, ey = 0;
-		int num = 8;
-		float divAngle = 3.141592f * 2 / num;
-		static float angle = 0;
-		angle += 0.01f;
-		for (int i = 0; i < num; i++) {
-			ex = cos(angle + divAngle * i) * radius;
-			ey = sin(angle + divAngle * i) * radius;
-			//mathLine(sx, sy, ex, ey, thickness);
-			//mathCircle(ex, ey, diameter*13);
-			sx = ex;
-			sy = ey;
-		}
+		fill(0, 0.678f, 0.662f);
+		mathGraph(func4, increments, thickness, plotsize);
 
 		present();
 	}

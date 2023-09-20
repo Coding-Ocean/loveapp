bffe7400bdfa1739b75d4ebac17ef3e7272d53cc
Coding-Ocean <eijo524@gmail.com>
2023-09-20 18:02:06 +0900

    add user font

--- loveapp.cpp
+++ loveapp.cpp
@@ -1,76 +1,53 @@
 #include"../lovelib/lovelib.h"
 
-#include<cstdio>
-void fontFace(const char* fontname, unsigned char charset = SHIFTJIS_CHARSET);
-void fontSize(int size);
-void printInit();
-void print(const char* format, ...);
-void destroyFontMap();
-void text(const char* str, float x, float y);
-
-#include<string>
-class USER_FONT
-{
-private:
-	std::string Filename;
-public:
-	USER_FONT(const char* filename)
-		:Filename(filename)
-	{
-		AddFontResourceEx(filename, FR_PRIVATE, 0);
-	}
-	~USER_FONT() {
-		RemoveFontResourceEx(Filename.c_str(), FR_PRIVATE, 0);
-	}
-};
-
 void gmain()
 {
 	window("Love", 1920, 1080);
 
-	//USER_FONT user1("PixelMplus12-Bold.ttf");
-	USER_FONT user2("KFhimajiFUDE.otf");
-	
 	int cnt = 0;
 
 	while (!quit()) {
 		getInputState();
 		if (isTrigger(KEY_ESC)) closeWindow();
-		
-		cnt++;
 
 		clear(0, 0.1f, 0.1f);
-		printInit();
 
-
-		fontSize(20);
-		fill(1,1,1);
-
-		//fontFace("ＫＦひま字ふで");
-		//print("エヴァンゲリオン");
-
-		//fontFace("HGP明朝E");
-		//print("時に、西暦2017年");
-		//
-		//text("使徒、襲来", mouseX, mouseY);
-		//
-		//fontFace("PixelMplus12");
-		//print("ゆうしゃはたおれた");
-
-		//fontFace("あずきフォント");
-		//fontSize(100);
-		//print("「%d番、習近平と申します」",cnt);
-		
-
-		//fontFace("ＫＦひま字ふで");
-		print("cnt:%d", cnt);
-		//fontFace("Comic Sans MS", 0);
-		print("Welcome to Coding Ocean!");
-		//fontFace("jokerman", 0);
-		print("Welcome to Coding Ocean!");
+		fontFace("HGP明朝E");
+		fontSize(100);
+		for (int i = 0; i < 10; i++) {
+			fill(1, 0.1f*i, 1);
+			text("使徒、襲来",900,i*100);
+		}
 
 		present();
 	}
-
-	destroyFontMap();
 }
+
+/*
+		fontFace("HGP明朝E");
+		fontSize(250);
+		fill(1,1,1);
+		print("エヴァンゲリオン");
+		print("時に、西暦%04.0f年",mouseX);
+		print("使徒、襲来");
+
+		fontFace("あずきフォント");
+		fill(1, 1, 0.5f);
+		text("習近平", mouseX, 800);
+
+	USER_FONT user1("PixelMplus12-Bold.ttf");
+	USER_FONT user2("KFhimajiFUDE.otf");
+		fontFace("ＫＦひま字ふで");
+		fontSize(80);
+		print("あけましておめでたう:%d", cnt);
+
+		fontFace("PixelMplus12");
+		print("ゆうしゃは瀕死");
+
+		fontFace("Comic Sans MS", 0);
+		fontSize(100);
+		print("Welcome to Coding Ocean!");
+
+		fontFace("jokerman", 0);
+		print("Welcome to Coding Ocean!");
+*/

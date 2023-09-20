ed29e02851e92c4b82b1ca5eeb8d258d4edb6ac6
Coding-Ocean <eijo524@gmail.com>
2023-09-20 14:20:18 +0900

    add text

--- loveapp.cpp
+++ loveapp.cpp
@@ -1,17 +1,36 @@
 #include"../lovelib/lovelib.h"
 
 #include<cstdio>
+void fontFace(const char* fontname, unsigned char charset = SHIFTJIS_CHARSET);
 void fontSize(int size);
 void printInit();
 void print(const char* format, ...);
 void destroyFontMap();
-void fontFace(const char* fontname);
+void text(const char* str, float x, float y);
+
+#include<string>
+class USER_FONT
+{
+private:
+	std::string Filename;
+public:
+	USER_FONT(const char* filename)
+		:Filename(filename)
+	{
+		AddFontResourceEx(filename, FR_PRIVATE, 0);
+	}
+	~USER_FONT() {
+		RemoveFontResourceEx(Filename.c_str(), FR_PRIVATE, 0);
+	}
+};
 
 void gmain()
 {
 	window("Love", 1920, 1080);
 
-
+	//USER_FONT user1("PixelMplus12-Bold.ttf");
+	USER_FONT user2("KFhimajiFUDE.otf");
+	
 	int cnt = 0;
 
 	while (!quit()) {
@@ -23,20 +42,35 @@ void gmain()
 		clear(0, 0.1f, 0.1f);
 		printInit();
 
-		fontSize(150);
 
-		fontFace("HGP明朝E");
+		fontSize(20);
 		fill(1,1,1);
-		print("エヴァンゲリオン");
-		print("時に、西暦2017年");
-		print("使徒襲来:%d", cnt);
+
+		//fontFace("ＫＦひま字ふで");
+		//print("エヴァンゲリオン");
+
+		//fontFace("HGP明朝E");
+		//print("時に、西暦2017年");
+		//
+		//text("使徒、襲来", mouseX, mouseY);
+		//
+		//fontFace("PixelMplus12");
+		//print("ゆうしゃはたおれた");
+
+		//fontFace("あずきフォント");
+		//fontSize(100);
+		//print("「%d番、習近平と申します」",cnt);
 		
-		fontFace("あずきフォント");
-		print("習近平");
+
+		//fontFace("ＫＦひま字ふで");
+		print("cnt:%d", cnt);
+		//fontFace("Comic Sans MS", 0);
+		print("Welcome to Coding Ocean!");
+		//fontFace("jokerman", 0);
+		print("Welcome to Coding Ocean!");
 
 		present();
 	}
 
 	destroyFontMap();
-
 }

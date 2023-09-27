86083872570a7869f67fb3b9f77938205e5d92a2
Coding-Ocean <eijo524@gmail.com>
2023-09-27 15:50:33 +0900

    createTexture v1.2

--- loveapp.cpp
+++ loveapp.cpp
@@ -1,55 +1,76 @@
-#if 1
-#include"../lovelib/lovelib.h"
-void gmain()
-{
-	window("Love", 1920, 1080);
-
-	//fontFace("Comic Sans MS", ANSI_CHARSET);
-	//fontFace("jokerman", ANSI_CHARSET);
-	//fontFace("Showcard Gothic",ANSI_CHARSET);
-	//fontFace("Harlow Solid Italic", ANSI_CHARSET);
-
-	fontSize(150);
-
-	while (!quit()) {
-		getInputState();
-		if (isTrigger(KEY_ESC)) closeWindow();
-
-		clear(0, 0.1f, 0.1f);
-		fill(0,0.5f,1);
-		print("Welcome to Coding Ocean!");
-		for (int i = 0; i < 3; i++) {
-			if (i == 0)
-				fill(1, 0, 0);
-			else if (i == 1)
-				fill(0, 1, 0);
-			else 
-				fill(1, 1, 0);
-			print("AKB%d", i + 46);
-		}
-		present();
-	}
-}
-#endif
+//text関数
 #if 0
 #include"../lovelib/lovelib.h"
 void gmain()
 {
 	window("Love", 1920, 1080);
 
-	USER_FONT userFont("KFhimajiFUDE.otf");
-	fontFace("ＫＦひま字ふで");
-	fontSize(150);
+	while (!quit()) {
+		getInputState();
+		if (isTrigger(KEY_ESC)) closeWindow();
+
+		clear(0,0,0);
+
+		present();
+	}
+}
+#endif
+//print関数
+#if 0
+#include"../lovelib/lovelib.h"
+void gmain()
+{
+	window("Love", 1920, 1080);
 
 	while (!quit()) {
 		getInputState();
 		if (isTrigger(KEY_ESC)) closeWindow();
 
-		clear(0, 0.1f, 0.1f);
-		print("あるところに");
-		print("おじいさんと");
-		print("おばあさんが");
-		print("しんでいました");
+		clear(0, 0, 0);
+
+		present();
+	}
+}
+#endif
+//フォントフェイスを変える
+#if 1
+#include"../lovelib/lovelib.h"
+void gmain()
+{
+	window("Love", 1920, 1080);
+
+	while (!quit()) {
+		getInputState();
+		if (isTrigger(KEY_ESC)) closeWindow();
+
+		clear(0, 0, 0);
+		fontFace("HGP明朝E");
+		fontSize(250);
+		fill(1, 1, 1);
+		print("エヴァンゲリオン");
+		print("時に、西暦%04.0f年", mouseX);
+		print("使徒、襲来");
+
+		fontFace("あずきフォント");
+		fill(1, 1, 0.5f);
+		text("習近平", mouseX, 800);
+		present();
+	}
+}
+#endif
+//インストールされていないフォントを使う
+#if 0
+#include"../lovelib/lovelib.h"
+void gmain()
+{
+	window("Love", 1920, 1080);
+
+	while (!quit()) {
+		getInputState();
+		if (isTrigger(KEY_ESC)) closeWindow();
+
+		clear(0, 0, 0);
+
 		present();
 	}
 }
@@ -71,29 +92,30 @@ void gmain()
 		text("    printf(\"Hello\");", x, y); y += size;
 		text("}", x, y); y += size;
 
+		fontFace("Harlow Solid Italic",ANSI_CHARSET);
+		fontFace("Showcard Gothic",ANSI_CHARSET);
+		fontFace("Comic Sans MS", ANSI_CHARSET);
+		fontFace("jokerman", ANSI_CHARSET);
+
 		fontFace("HGP明朝E");
 		fontSize(250);
 		fill(1,1,1);
 		print("エヴァンゲリオン");
 		print("時に、西暦%04.0f年",mouseX);
 		print("使徒、襲来");
-
 		fontFace("あずきフォント");
 		fill(1, 1, 0.5f);
 		text("習近平", mouseX, 800);
 
-	USER_FONT user2("KFhimajiFUDE.otf");
+		USER_FONT user1("KFhimajiFUDE.otf");
 		fontFace("ＫＦひま字ふで");
 		fontSize(80);
-		print("あけましておめでたう:%d", cnt);
+		print("エヴァンゲリオン:%d", cnt);
 
-	USER_FONT user1("PixelMplus12-Bold.ttf");
+		USER_FONT user2("PixelMplus12-Bold.ttf");
 		fontFace("PixelMplus12");
 		fontSize(100);
 		print("ゆうしゃはいきかえった");
 
-	fontFace("Harlow Solid Italic",ANSI_CHARSET);
-	fontFace("Showcard Gothic",ANSI_CHARSET);
-	fontFace("Comic Sans MS", ANSI_CHARSET);
-	fontFace("jokerman", ANSI_CHARSET);
 */
+

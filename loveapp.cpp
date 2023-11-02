496d5a4d3bb4032b32d4d6369eb4856bacec80c7
Coding-Ocean <eijo524@gmail.com>
2023-11-02 14:45:12 +0900

    mod output vertex structure

--- loveapp.cpp
+++ loveapp.cpp
@@ -10,36 +10,37 @@ void gmain()
 	const int numY = 8;//縦方向頂点数
 	const int numVertices = numY * numX;//全頂点数
 
-	//左側頂点の位置。このX座標ににオフセットを足して全位置を設定していく
+	//最左端頂点の位置。このX座標ににオフセットを足して全位置を設定していく
 	float l = 0.2f;
 	VEC leftP[numY] = {
-		VEC(-1,-l,-l),//前下
-		VEC(-1, l,-l),//前上
-		VEC(-1, l,-l),//上前
-		VEC(-1, l, l),//上後
-		VEC(-1, l, l),//後上
-		VEC(-1,-l, l),//後下
-		VEC(-1,-l, l),//下後
-		VEC(-1,-l,-l),//下前
+		VEC(-1,-l,-l),//前面下
+		VEC(-1, l,-l),//前面上
+		VEC(-1, l,-l),//上面前
+		VEC(-1, l, l),//上面後
+		VEC(-1, l, l),//後面上
+		VEC(-1,-l, l),//後面下
+		VEC(-1,-l, l),//下面後
+		VEC(-1,-l,-l),//下面前
 	};
-	//左側頂点の法線。全部同じなのでコピーするだけ。
+	//最左端頂点の法線。全部同じなので右方向の頂点にコピーしていくだけ。
 	VEC leftN[numY] = {
-		VEC(0,0,-1),//前
+		VEC(0,0,-1),//前面
 		VEC(0,0,-1),
-		VEC(0,1,0),//上
+		VEC(0,1,0),//上面
 		VEC(0,1,0),
-		VEC(0,0,1),//後
+		VEC(0,0,1),//後面
 		VEC(0,0,1),
-		VEC(0,-1,0),//下
+		VEC(0,-1,0),//下面
 		VEC(0,-1,0),
 	};
 
-	//座標変換前の頂点要素（定数）
+	//座標変換前の頂点要素の入れ物（定数）
+	//読みやすくするため、あえて構造体にしていない!!
 	VEC   p[numVertices]{};//position 位置
 	VEC   n[numVertices]{};//normal 法線
-	float w0[numVertices]{};//weight0 重み0（行列が影響する割合0）
-	float w1[numVertices]{};//weight1 重み1（行列が影響する割合1）
-	//p,n,wを設定
+	float w0[numVertices]{};//weight0 重み0（行列0が影響する割合）
+	float w1[numVertices]{};//weight1 重み1（行列1が影響する割合）
+	//p, n, w0, w1を設定
 	float ofstX = 2.0f / (numX - 1);
 	float ofstW = 1.0f / (numX - 1);
 	for (int i = 0; i < numX; i++) {
@@ -57,32 +58,38 @@ void gmain()
 		}
 	}
 
-	//インデックスをつくる
+	//三角形数とインデックス数
 	const int numTriangles = numY * (numX - 1);//1列は８つの三角形
 	const int numIndices = numTriangles * 3;
+	//インデックスの入れ物
 	unsigned indices[numIndices]{};
+	//インデックスを設定
 	int j = 0;
 	for (int i = 0; i < numTriangles/2; i++) {
 		int k = i * 2;
-		//三角形１
+		//左上の三角形のインデックス
 		indices[j++] = k + 0; 
 		indices[j++] = k + 9; 
 		indices[j++] = k + 1;
-		//三角形２
+		//右下の三角形のインデックス
 		indices[j++] = k + 0; 
 		indices[j++] = k + 8; 
 		indices[j++] = k + 9;
 	}
 	
 	//ワールド座標変換行列２つ
-	MAT mat0, mat1;
+	MAT mat0;//この行列が頂点や法線に影響する割合が"w0"
+	MAT mat1;//この行列が頂点や法線に影響する割合が"w1"
 
-	//座標変換後の位置と法線
-	VEC p_[numVertices];
-	VEC n_[numVertices];
-
-	//描画用頂点pos,normal,uvを持つ。座標変換後ここにセットする。
-	VERTEX vertices[numVertices]{};
+	//座標返還後の値を入れる頂点の構造体
+	struct VERTEX_ {
+		VEC p;
+		VEC n;
+		float u, v;
+	};
+	//座標変換後の値を入れる頂点群
+	VERTEX_ vertices[numVertices]{};
+	//メンバp、nにはメインループ内で座標返還後の値をセットする
 	//uvは座標変換に影響を受けないのでここで入れておく
 	float ofstU = 1.0f / (numX - 1);
 	for (int i = 0; i < numX; i++) {
@@ -93,7 +100,7 @@ void gmain()
 	}
 
 	//テクスチャ読み込み
-	int tex = loadImage("silver.png");
+	int textureId = loadImage("silver.png");
 
 	//三角関数用ラジアン
 	float rad = 0;
@@ -103,6 +110,7 @@ void gmain()
 
 	specularOn();
 	
+	//メインループ。座標変換と描画を繰り返す
 	while (!quit()) {
 		getInputState();
 		if (isTrigger(KEY_ESC)) closeWindow();
@@ -115,9 +123,9 @@ void gmain()
 		}
 
 		//ひねるラジアン値
-		float r0 = sinf(rad) * 1.5f;
-		float r1 = cosf(rad) * 1.5f;
-		rad += 0.03f;
+		float r0 = sinf(rad*2) * 1.5f;
+		float r1 = cosf(rad*3) * 2.0f;
+		rad += 0.005f;
 
 		//行列をつくる
 		mat0.identity();
@@ -128,23 +136,16 @@ void gmain()
 		//座標変換
 		for (int i = 0; i < numVertices; i++) {
 			//位置。行列計算後の位置にWeight(行列が影響する割合)を掛ける
-			p_[i] = mul(p[i], mat0) * w0[i];
-			p_[i] += mul(p[i], mat1) * w1[i];
+			vertices[i].p = mul(p[i], mat0) * w0[i];
+			vertices[i].p += mul(p[i], mat1) * w1[i];
 			//法線ベクトル。位置と同様の計算をする
-			n_[i] = mul(n[i], mat0) * w0[i];
-			n_[i] += mul(n[i], mat1) * w1[i];
-			//変換後、描画用頂点にコピー
-			vertices[i].x = p_[i].x;
-			vertices[i].y = p_[i].y;
-			vertices[i].z = p_[i].z;
-			vertices[i].nx = n_[i].x;
-			vertices[i].ny = n_[i].y;
-			vertices[i].nz = n_[i].z;
+			vertices[i].n = mul(n[i], mat0) * w0[i];
+			vertices[i].n += mul(n[i], mat1) * w1[i];
 		}
 
 		//描画
-		clear(0.f, 0.f, 0.f);
-		model(vertices, indices, numTriangles, tex);
+		clear(0.1f, 0.1f, 0.1f);
+		model_(vertices, indices, numTriangles, textureId);
 		present();
 	}
 }
